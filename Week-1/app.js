152af9c76037126c0a6c3372fb820db65e7143af
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-01-30 18:56:48 -0500

    week 1

---
+++ Week-1/app.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Take out Trash</p>
+            
+            <input type="checkbox" />
+            <p>Do the dishes</p>
+            
+            <input type="checkbox" />
+            <p>vaccuum</p>
+            
+    
+        </div>
+    )
+}
+
+export default App
