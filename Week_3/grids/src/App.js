6f86c37f99154a52e59015777b00f57a0b780e2d
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-02-18 19:50:05 -0500

    week3

---
+++ Week_3/grids/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+handleClick(){
+ //need help with event handlers and state
+}
+
+function App() {
+  return (
+    <div className="grid-container">
+
+    <div class="grid-item1">
+      <button onClick = {handleClick}>one</button>
+    </div>
+
+    <div class="grid-item2">
+      <button onClick = {handleClick}>two</button>
+    </div>
+
+    <div class="grid-item3">
+      <button onClick = {handleClick}>three</button>
+      </div>  
+
+    <div class="grid-item4">
+      <button onClick = {handleClick}>four</button>
+    </div>
+    
+
+</div>
+  );
+}
+
+export default App;
