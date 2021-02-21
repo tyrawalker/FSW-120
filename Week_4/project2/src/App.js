a3fa8da567b516054acc6720f108ab110f152374
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-02-20 22:01:38 -0500

    placeholder

---
+++ Week_4/project2/src/App.js
@@ -0,0 +1,38 @@
+
+//placeholder. will resubmit for mastery
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+          name: "",
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+    
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text" 
+                    value={this.state.name} 
+                    name="name" 
+                    placeholder="Name" 
+                    onChange={this.handleChange} 
+                />
+
+                <h1>{this.state.firstName}</h1>
+            </form>
+        )
+    }
+}
+
+export default App
